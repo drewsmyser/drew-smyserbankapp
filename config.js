c81f969b65321a3713fa7cd40c44645df6d397f0
drewsmyser <85514911+drewsmyser@users.noreply.github.com>
2022-03-16 20:25:45 -0500

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  JWT_SECRET: "my super secret key",
-  MONGODB_URI:
-    "mongodb+srv://strudel:strudelbaby@strudz.odpuc.mongodb.net/sample_mflix?retryWrites=true&w=majority",
-}
